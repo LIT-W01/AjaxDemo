1987cdb971210bc664ae19cffa43ecb8b40adc25
Avrumi Friedman <info@lakewoodprogramming.com>
2016-02-17 20:49:11 -0500

    Initial commit

---
+++ MvcApplication12/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication12.Controllers
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        private static string[] _firstNames =
+        {
+            "Avrumi", "Chaim", "Yechiel", "Tzvi", "Suchi", "Shea", "Elkanah",
+            "Nechemia", "Yudi", "Sholom"
+        };
+
+        private static string[] _lastNames =
+        {
+            "Friedman", "Friedman", "Cohen", "Shoop", "Waldman", "Weidberg", "Werther",
+            "Schorr", "Goldgrab", "Rechnitz"
+        };
+
+        private static Random _rnd = new Random();
+        public static IEnumerable<Person> GetRandomPeople(int amount)
+        {
+            return Enumerable.Range(1, amount).Select(_ => new Person
+            {
+                FirstName = _firstNames[_rnd.Next(_firstNames.Length)],
+                LastName = _lastNames[_rnd.Next(_lastNames.Length)],
+                Age = _rnd.Next(20, 65)
+            });
+        }
+    }
+
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult GetPeople(int peopleAmount)
+        {
+            IEnumerable<Person> ppl = Person.GetRandomPeople(peopleAmount);
+            return Json(ppl, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ MvcApplication12/Scripts/index.js
@@ -0,0 +1,16 @@
+﻿$(function () {
+    $("#getpeople-button").on('click', function () {
+        var amt = $("#pplAmount").val();
+        $.post("/home/getpeople", { peopleAmount: amt }, function (people) {
+            people.forEach(function (person) {
+                var row = $("<tr><td>" + person.FirstName +
+                    "</td><td>" + person.LastName + "</td><td>" + person.Age + "</td></tr>");
+                $("table").append(row);
+            });
+        });
+    });
+
+    $("#clear-button").on('click', function() {
+        $("table tr:gt(0)").remove();
+    });
+});
